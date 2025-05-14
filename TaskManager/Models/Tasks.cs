ce361c76a5f243561a09d6f0541466d70b123e9f
rajeshchaudhari7778 <rajesh.chaudhari@azilen.com>
2025-05-14 14:12:32 +0530

    Add DueDate to Task model

--- TaskManager/Models/Tasks.cs
+++ TaskManager/Models/Tasks.cs
@@ -14,5 +14,7 @@ namespace TaskManager.Models
         public bool IsCompleted { get; set; }
 
         public DateTime CreatedAt { get; set; }
+
+        public DateTime? DueDate { get; set; }
     }
 }
